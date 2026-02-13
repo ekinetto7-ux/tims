9101a553463193fda43cf7a2908139eda5e630b9
ekinetto7-ux <ekinetto7@gmail.com>
2026-02-14 00:21:50 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const melody = document.getElementById("melody");
 
-function playKimitsu() {
-    melody.src = "Verde_Rayo_endless.mp3";
-    melody.pause();
-    melody.currentTime = 10;
-    melody.play().catch(()=>{});
+function playKimitsu(){
+  melody.src = "Verde_Rayo_endless.mp3";
+  melody.currentTime = 10;
+  melody.play().catch(()=>{});
 }
 
-function stopMelody() {
-    melody.pause();
-    melody.currentTime = 0;
+function playKimitsu2(){
+  melody.src = "Verde_Rayo.mp3";
+  melody.currentTime = 0;
+  melody.play().catch(()=>{});
+}
+
+function stopMelody(){
+  melody.pause();
+  melody.currentTime = 0;
 }
